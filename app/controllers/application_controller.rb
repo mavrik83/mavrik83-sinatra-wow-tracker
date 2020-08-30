cc5fb60383dbb54dc9e908b398bf59a41f1417a8
mavrik83 <rjhuber83@gmail.com>
2020-08-30 11:35:44 -0400

    adds application_controller and Rakefile

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+    configure do
+        set :public_folder, 'pubic'
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, "trytofigurethisout"
+    end
+
+
+
+end
