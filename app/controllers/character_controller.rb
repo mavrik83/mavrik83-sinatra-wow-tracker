46176f952c8cb13ba2e8aefdb68bfb934b5953ae
mavrik83 <rjhuber83@gmail.com>
2020-08-30 12:43:40 -0400

    sets up routes

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,4 +1,11 @@
 class CharacterController < ApplicationController
 
-    
+    get '/characters' do
+        if logged_in?
+            @characters = Character.all
+            erb :'users/show'
+        else
+            redirect to '/'
+    end
+
 end
