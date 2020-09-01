08f82deaac6e02e0a6124477b0b04869ce118961
mavrik83 <rjhuber83@gmail.com>
2020-09-01 01:19:04 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+u1 = User.create(username: 'Ryan', email: 'ryan@gmail.com', password: 'password')
+Character.create(name: 'argari', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: u1.id)
+Character.create(name: 'tella', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: u1.id)
+Character.create(name: 'mimi', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: u1.id)
+Character.create(name: 'jadase', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: u1.id)
 
-Character.create(name: 'argari', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: 2)
-Character.create(name: 'tella', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: 2)
-Character.create(name: 'mimi', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: 2)
-Character.create(name: 'jadase', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: 2)
-
+u2 = User.create(username: 'Ril', email: 'ril@gmail.com', password: 'password')
+Character.create(name: 'charlie', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: u2.id)
+Character.create(name: 'malia', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: u2.id)
