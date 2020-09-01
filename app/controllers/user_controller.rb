2a68cf70baa8b538687aa5bb9cafc7d5205d45d8
mavrik83 <rjhuber83@gmail.com>
2020-09-01 09:20:52 -0400

    updates the login logic

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -37,6 +37,16 @@ class UserController < ApplicationController
       end
     end
     
+    post '/login' do
+      user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect to "/characters"
+      else
+        redirect to '/'
+      end
+    end
+  
     get '/logout' do
       if logged_in?
         session.destroy
