6f013420c8942a5bdb55d06cce5e588e7d66228f
mavrik83 <rjhuber83@gmail.com>
2020-09-01 09:18:19 -0400

    fixes formatting and spacing

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,53 +6,14 @@ class UserController < ApplicationController
     end
 
     get '/signup' do
-        if !logged_in?
-          erb :'users/create_user'
-        else
+      end
+    
           redirect to '/characters'
-        end
-      end
-    
-      post '/signup' do
-        if params[:username] == "" || params[:email] == "" || params[:password] == ""
-          redirect to '/signup'
-        else
-          @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-          if @user.save
-            session[:user_id] = @user.id
-            redirect to '/characters'
-          else
-            redirect to '/'
-          end
-        end
-      end
-    
-      get '/login' do
-        if !logged_in?
-          erb :'users/login'
-        else
-          redirect to '/characters'
-        end
-      end
-    
-      post '/login' do
-        user = User.find_by(:username => params[:username])
-        if user && user.authenticate(params[:password])
-          session[:user_id] = user.id
-          redirect to "/characters"
         else
           redirect to '/'
         end
       end
     
-      get '/logout' do
-        if logged_in?
-          session.destroy
-          redirect to '/'
-        else
-          redirect to '/'
-        end
       end
     
-
 end
