a59da9a973bb36ab18f26b1454e0272be94f9386
mavrik83 <rjhuber83@gmail.com>
2020-09-01 16:34:40 -0400

    Update character_controller.rb

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -43,28 +43,36 @@ class CharacterController < ApplicationController
     end
 
     get '/add_character' do
-        erb :'characters/add_character'
+        if logged_in?
+            erb :'characters/add_character'
+        else
+            redirect to '/'
+        end
     end
 
     post '/add_character' do
-        name = params[:name]
-        if ['!', '?', ';', '(', ')', '#'].any?{ |sym| name.include?(sym) } || name == ""
-            redirect to '/add_character' 
+        if logged_in?
+            name = params[:name]
+            if ['!', '?', ';', '(', ')', '#'].any?{ |sym| name.include?(sym) } || name == ""
+                redirect to '/add_character' 
+            else
+                @character = Character.new(
+                    :name => params[:name],
+                    :intellect => params[:intellect],
+                    :strength => params[:strength],
+                    :stamina => params[:stamina],
+                    :agility => params[:agility],
+                    :haste => params[:haste],
+                    :versatility => params[:versatility],
+                    :mastery => params[:mastery],
+                    :crit_chance => params[:crit_chance]
+                )
+                @character.user_id = current_user.id
+                @character.save
+                redirect to '/characters'
+            end
         else
-            @character = Character.new(
-                :name => params[:name],
-                :intellect => params[:intellect],
-                :strength => params[:strength],
-                :stamina => params[:stamina],
-                :agility => params[:agility],
-                :haste => params[:haste],
-                :versatility => params[:versatility],
-                :mastery => params[:mastery],
-                :crit_chance => params[:crit_chance]
-            )
-            @character.user_id = current_user.id
-            @character.save
-            redirect to '/characters'
+            redirect to '/'
         end
     end
 
@@ -82,24 +90,28 @@ class CharacterController < ApplicationController
     end
 
     post '/characters/:id/edit' do
-        name = params[:name]
-        if ['!', '?', ';', '(', ')', '#'].any?{ |sym| name.include?(sym) } || name == ""
-            redirect to "/character/#{params[:id]}/edit"      
+        if logged_in?
+            name = params[:name]
+            if ['!', '?', ';', '(', ')', '#'].any?{ |sym| name.include?(sym) } || name == ""
+                redirect to "/character/#{params[:id]}/edit"      
+            else
+                @character = Character.find(params[:id])
+                @character.update(
+                    :name => params[:name],
+                    :intellect => params[:intellect],
+                    :strength => params[:strength],
+                    :stamina => params[:stamina],
+                    :agility => params[:agility],
+                    :haste => params[:haste],
+                    :versatility => params[:versatility],
+                    :mastery => params[:mastery],
+                    :crit_chance => params[:crit_chance]
+                )
+                @character.save
+                redirect to '/characters'
+            end
         else
-            @character = Character.find(params[:id])
-            @character.update(
-                :name => params[:name],
-                :intellect => params[:intellect],
-                :strength => params[:strength],
-                :stamina => params[:stamina],
-                :agility => params[:agility],
-                :haste => params[:haste],
-                :versatility => params[:versatility],
-                :mastery => params[:mastery],
-                :crit_chance => params[:crit_chance]
-            )
-            @character.save
-            redirect to '/characters'
+            redirect to '/'
         end
     end
 end
