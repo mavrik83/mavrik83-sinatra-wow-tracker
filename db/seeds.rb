d3ef34d776844a686e56ae6fb3d674441d7e18d4
mavrik83 <rjhuber83@gmail.com>
2020-09-01 14:14:52 -0400

    adds seed data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@ Character.create(name: 'jadase', intellect: 10, strength: 10, stamina: 10, agili
 u2 = User.create(username: 'Ril', email: 'ril@gmail.com', password: 'password')
 Character.create(name: 'charlie', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: u2.id)
 Character.create(name: 'malia', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: u2.id)
+
+u3 = User.create(username: 'Thea', email: 'thea@gmail.com', password: 'password')
+Character.create(name: 'eero', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: u3.id)
+Character.create(name: 'schuyler', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: u3.id)
