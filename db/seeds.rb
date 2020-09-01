efc9ef6149a4f86bf2016cd1edd8fc7c4111d936
mavrik83 <rjhuber83@gmail.com>
2020-08-31 20:35:19 -0400

    Create seeds.rb

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+
+Character.create(name: 'argari', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: 2)
+Character.create(name: 'tella', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: 2)
+Character.create(name: 'mimi', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: 2)
+Character.create(name: 'jadase', intellect: 10, strength: 10, stamina: 10, agility: 10, haste: 10, versatility: 10, mastery: 10, crit_chance: 10, user_id: 2)
+
