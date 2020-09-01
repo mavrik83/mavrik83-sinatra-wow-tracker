fd4f016d65727a009ca9670a715dbc4baeb8382a
mavrik83 <rjhuber83@gmail.com>
2020-08-31 20:31:14 -0400

    Update character_controller.rb

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,14 +1,33 @@
 class CharacterController < ApplicationController
 
+    get '/characters' do
+        if logged_in?
+            # .characters is an active record method that was auto 
+            # created by the model having the correct has_many and belongs_to
+            @characters = current_user.characters
+            # @characters = Character.where(user_id)
+            erb :'characters/characters'
+        else
+            redirect to '/'
+        end
+
+    end
+    
     post '/characters' do
         if logged_in?
-            #@characters = Character.all
             erb :'characters/characters'
         else
             redirect to '/'
         end
     end
 
-
+    get '/character/:id' do
+        if logged_in?
+            @character = Character.find(params[:id])
+            erb :'characters/show_character'
+        else
+            redirect to '/'
+        end
+    end
 
 end
